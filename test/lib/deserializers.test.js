9c6cbdcb34d3766edbd7c90eeef62422f237c859
Tim Schaub <tim.schaub@gmail.com>
2014-06-16 09:28:22 -0600

    Deserializers

---
+++ test/lib/deserializers.test.js
@@ -0,0 +1,177 @@
+var lab = require('lab');
+
+var deserializers = require('../../lib/deserializers');
+
+var assert = lab.assert;
+var experiment = lab.experiment;
+var test = lab.test;
+
+
+experiment('deserializers', function() {
+
+  experiment('get()', function() {
+    var get = deserializers.get;
+
+    test('returns a function for a known type', function(done) {
+      var deserialize = get('string');
+      assert.isFunction(deserialize);
+      done();
+    });
+
+    test('throws for an unknown type', function(done) {
+      assert.throws(function() {
+        get('foo');
+      }, 'Unable to deserialize type: foo');
+      done();
+    });
+
+    test('returns an appropriate deserializer for string', function(done) {
+      var deserialize = get('string');
+      assert.equal(deserialize('foo'), 'foo');
+      done();
+    });
+
+    test('string deserializer throws for non-string', function(done) {
+      var deserialize = get('string');
+      assert.throws(function() {
+        deserialize(10);
+      }, 'Expected string to deserialize: 10');
+      done();
+    });
+
+    test('string deserializer throws for empty string', function(done) {
+      var deserialize = get('string');
+      assert.throws(function() {
+        deserialize('');
+      }, 'Expected string to deserialize: ');
+      done();
+    });
+
+    test('returns an appropriate deserializer for number', function(done) {
+      var deserialize = get('number');
+      assert.equal(deserialize('42'), 42);
+      done();
+    });
+
+    test('number deserializer throws for non-string', function(done) {
+      var deserialize = get('number');
+      assert.throws(function() {
+        deserialize([]);
+      }, 'Expected string to deserialize: ');
+      done();
+    });
+
+    test('number deserializer throws for empty string', function(done) {
+      var deserialize = get('number');
+      assert.throws(function() {
+        deserialize('');
+      }, 'Expected string to deserialize: ');
+      done();
+    });
+
+    test('number deserializer throws for non-numeric string', function(done) {
+      var deserialize = get('number');
+      assert.throws(function() {
+        deserialize('foo');
+      }, 'Expected to deserialize a number: foo');
+      done();
+    });
+
+    test('returns an appropriate deserializer for date', function(done) {
+      var deserialize = get('date');
+      var then = '2014-06-09T23:57:12.588Z';
+      var date = deserialize(then);
+      assert.instanceOf(date, Date);
+      assert.equal(date.getTime(), new Date(then).getTime());
+      done();
+    });
+
+    test('date deserializer throws for non string', function(done) {
+      var deserialize = get('date');
+      assert.throws(function() {
+        deserialize(10);
+      }, 'Expected string to deserialize: 10');
+      done();
+    });
+
+    test('date deserializer throws for empty string', function(done) {
+      var deserialize = get('date');
+      assert.throws(function() {
+        deserialize('');
+      }, 'Expected string to deserialize: ');
+      done();
+    });
+
+    test('date deserializer throws for invalid date string', function(done) {
+      var deserialize = get('date');
+      assert.throws(function() {
+        deserialize('foo');
+      }, 'Expected to deserialize a date: foo');
+      done();
+    });
+
+    test('returns an appropriate serializer for array', function(done) {
+      var deserialize = get('array');
+      assert.deepEqual(deserialize('["bar", 100]'), ['bar', 100]);
+      done();
+    });
+
+    test('array deserializer throws for non string', function(done) {
+      var deserialize = get('array');
+      assert.throws(function() {
+        deserialize(10);
+      }, 'Expected string to deserialize: 10');
+      done();
+    });
+
+    test('array deserializer throws for empty string', function(done) {
+      var deserialize = get('array');
+      assert.throws(function() {
+        deserialize('');
+      }, 'Expected string to deserialize: ');
+      done();
+    });
+
+    test('array deserializer throws for invalid array string', function(done) {
+      var deserialize = get('array');
+      assert.throws(function() {
+        deserialize('foo');
+      }, 'Expected to deserialize an array: foo');
+      done();
+    });
+
+    test('returns an appropriate deserializer for object', function(done) {
+      var deserialize = get('object');
+      var json = '{"foo": "bar"}';
+      var obj = deserialize(json);
+      assert.deepEqual(obj, {foo: 'bar'});
+      done();
+    });
+
+    test('object deserializer throws for non string', function(done) {
+      var deserialize = get('object');
+      assert.throws(function() {
+        deserialize(10);
+      }, 'Expected string to deserialize: 10');
+      done();
+    });
+
+    test('object deserializer throws for empty string', function(done) {
+      var deserialize = get('object');
+      assert.throws(function() {
+        deserialize('');
+      }, 'Expected string to deserialize: ');
+      done();
+    });
+
+    test('object deserializer throws for invalid object', function(done) {
+      var deserialize = get('object');
+      assert.throws(function() {
+        deserialize('foo');
+      }, 'Expected to deserialize an object: foo');
+      done();
+    });
+
+  });
+
+});
---
+++ test/lib/util.test.js
@@ -0,0 +1,68 @@
+var lab = require('lab');
+
+var util = require('../../lib/util');
+
+var assert = lab.assert;
+var experiment = lab.experiment;
+var test = lab.test;
+
+experiment('util', function() {
+
+  experiment('typeOf()', function() {
+    var typeOf = util.typeOf;
+
+    test('returns "number" for numbers', function(done) {
+      assert.equal('number', typeOf(42));
+      assert.equal('number', typeOf(0));
+      done();
+    });
+
+    test('returns "string" for strings', function(done) {
+      assert.equal('string', typeOf('foo'));
+      assert.equal('string', typeOf(''));
+      done();
+    });
+
+    test('returns "array" for array', function(done) {
+      assert.equal('array', typeOf(['foo', 'bar']));
+      assert.equal('array', typeOf([]));
+      done();
+    });
+
+    test('returns "date" for date', function(done) {
+      assert.equal('date', typeOf(new Date()));
+      assert.equal('date', typeOf(new Date(0)));
+      done();
+    });
+
+    test('returns "regexp" for RegExp', function(done) {
+      assert.equal('regexp', typeOf(/foo/));
+      assert.equal('regexp', typeOf(new RegExp('foo')));
+      done();
+    });
+
+    test('returns "error" for Error', function(done) {
+      assert.equal('error', typeOf(new Error('foo')));
+      assert.equal('error', typeOf(new Error()));
+      done();
+    });
+
+    test('returns "object" for object', function(done) {
+      assert.equal('object', typeOf({}));
+      done();
+    });
+
+    test('returns "null" for null', function(done) {
+      assert.equal('null', typeOf(null));
+      done();
+    });
+
+    test('returns "undefined" for undefined', function(done) {
+      assert.equal('undefined', typeOf());
+      assert.equal('undefined', typeOf(undefined));
+      done();
+    });
+
+  });
+
+});
